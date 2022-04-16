7e7215f2af76431ae694457b569345da99f40f56
LUIS FERNANDO FELIX MATA <a329610@uach.mx>
2022-04-15 18:43:44 -0600

    Registrar Usuario added@

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,24 @@
+
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/registrar">Registrar Usuario</router-link>
   </nav>
   <router-view/>
-  <RegistrarUsuario></RegistrarUsuario>
 </template>
 
 
 <script>
-import RegistrarUsuario from './components/RegistrarUsuario.vue';
+
 
 export default {
   name: 'app',
-  components: {
-    RegistrarUsuario
-  }
+
 }
 </script>
 
 <style>
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
