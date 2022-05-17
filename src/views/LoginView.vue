6229a8199675ab5208095cbaf353aec5c04897ad
Ruben Dominguez <Ruben-Dominguez>
2022-05-17 14:45:13 -0600

    Se agregan los fonts al App.vue, se completa la primera pantalla de login sin navbar

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -9,26 +9,33 @@
           <h1>Inicio de Sesión</h1>
           <div class="form-container">
             <b-form class="login-form" @submit="onSubmit" @reset="onReset">
-              <b-form-group id="input-group-1" label="Usuario" label-for="input-1">
+              <b-form-group id="input-group-email">
+                <label>Correo Electrónico</label>
                 <b-form-input
                   id="input-1"
-                  v-model="form.username"
+                  v-model="form.email"
                   type="text"
+                  placeholder="Correo Electrónico"
                   required
                 ></b-form-input>
               </b-form-group>
 
-              <b-form-group id="input-group-2" label="Contraseña" label-for="input-2">
+              <b-form-group id="input-group-password">
+                <label>Contraseña</label>
                 <b-form-input
                   id="input-2"
                   v-model="form.password"
                   type="password"
+                  placeholder="Ingresa tu contraseña"
                   required
                 ></b-form-input>
               </b-form-group>
 
-              <b-button type="submit" variant="primary">Inicio de Sesión</b-button>
-              <b-button variant="light">¿Olvidaste tu contraseña?</b-button>
+              <b-button-group vertical>
+                <b-button class="password-forget-button" variant="light">¿Olvidaste tu contraseña?</b-button>
+                <b-button class="login-button" type="submit" variant="primary">Inicio de Sesión</b-button>
+                
+              </b-button-group>
             </b-form>
           </div>
         </b-col>
@@ -42,7 +49,7 @@
     data() {
       return {
         form: {
-          username: '',
+          email: '',
           password: ''
         }
       }
@@ -60,6 +67,8 @@
   * {
     padding: 0;
     margin: 0;
+    font-family: 'Roboto', sans-serif;
+    color: black;
   }
 
   .padding-0{
@@ -68,8 +77,8 @@
 }
 
   .login-body {
-    background-color: #0DB5B5;
-    height: 100vh;
+    background-color: #ECDFF4;
+    height: 90vh;
   }
 
   .login-photo {
@@ -93,5 +102,49 @@
     border-radius: 0 20px 20px 0;
   }
 
+  .form-container{
+    width: 100%;
+    padding: 0 3rem 0 3rem;
+  }
+
+  h1 {
+    text-align: left;
+    font-size: 2rem;
+    padding: 0 3rem 3rem 3rem;
+    font-weight: 600;
+  }
+
+  label {
+    text-align: left !important;
+    width: 100%;
+    padding: .5rem 0 .5rem 0;
+  }
+
+  input {
+    height: 4rem;
+    color: rgb(102, 102, 102)
+  }
+
+  .login-button {
+    background-color: #8D2BC0;
+    border-color: #8D2BC0;
+    color: white;
+  }
+
+  .login-button:hover {
+    background-color: #560b7d;
+    border-color: #560b7d;
+  }
+
+  .password-forget-button {
+    background-color: white;
+    border-color: white;
+    color: black;
+  }
+
+  .password-forget-button:hover {
+    background-color: lightgrey;
+    border-color: lightgrey;
+  } 
 
 </style>
