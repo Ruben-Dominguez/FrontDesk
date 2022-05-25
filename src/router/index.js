5b2177f1fe377760d089f33e968cad647370ccc1
moonormal <a334259@uach.mx>
2022-05-25 03:31:59 -0600

    Se agrega gimnasios (incompleto)

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,17 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import RegistrarUsuario from '../components/RegistrarUsuario.vue'
 import LoginView from '../views/LoginView.vue'
+import GimnasiosView from '../views/GimnasiosView.vue'
+//import MembresiasView from '../views/MembresiasView.vue'
+//import EquipamientosView from '../views/EquipamientosView.vue'
 
 const routes = [
   {path: '/',name: 'home',component: HomeView},
   {path: '/registrar', name: 'RegistrarUsuario',component: RegistrarUsuario},
   {path: '/login', name: 'Login',component: LoginView},
+  {path: '/gimnasios', name: 'Gimnasios',component: GimnasiosView},
+  //{path: '/membresias', name: 'Membresias',component: MembresiasView},
+  //{path: '/equipamientos', name: 'Equipamientos',component: EquipamientosView},
 ]
 
 const router = createRouter({
