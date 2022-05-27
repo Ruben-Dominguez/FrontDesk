6494096147fc1dfc7f9eb1b53911e45d4c4d4e8a
LuisFerUACH <a329610@uach.mx>
2022-05-26 18:45:22 -0600

    Campaign view added

--- src/router/index.js
+++ src/router/index.js
@@ -4,9 +4,11 @@ import LoginView from '../views/LoginView.vue'
 import SociosView from '../views/SociosView.vue'
 import PromotionalCampaignView from '../views/PromotionalCampaignView.vue'
 import AddCampaignView from '../views/AddCampaignView.vue'
+import EditCampaignView from '../views/EditCampaignView.vue'
 import GimnasiosView from '../views/GimnasiosView.vue'
 //import MembresiasView from '../views/MembresiasView.vue'
 //import EquipamientosView from '../views/EquipamientosView.vue'
+
 
 const routes = [
   {path: '/registrar', name: 'RegistrarUsuario',component: RegistrarUsuario},
@@ -14,6 +16,7 @@ const routes = [
   {path: '/socio', name: 'Socio',component: SociosView},
   {path: '/campaign', name: 'PromotionalCampaign',component: PromotionalCampaignView},
   {path: '/campaign-add', name: 'AddCampaign',component: AddCampaignView},
+    {path: '/campaign-edit', name: 'EditCampaign',component: EditCampaignView},
   {path: '/gimnasios', name: 'Gimnasios',component: GimnasiosView},
   //{path: '/membresias', name: 'Membresias',component: MembresiasView},
   //{path: '/equipamientos', name: 'Equipamientos',component: EquipamientosView},
